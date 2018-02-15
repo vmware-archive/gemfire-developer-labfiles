6aef949b433923c65f766f411a5cfad17e7b4ad7
Mark Secrist <msecrist@pivotal.io>
2018-02-15 11:13:13 -0700

    Forgot to add in update ClientCacheHelper

--- lab-projects/client-application/src/main/java/io/pivotal/bookshop/dao/GemFireClientCacheHelper.java
+++ lab-projects/client-application/src/main/java/io/pivotal/bookshop/dao/GemFireClientCacheHelper.java
@@ -7,48 +7,71 @@ import org.apache.geode.cache.client.ClientRegionShortcut;
 
 import io.pivotal.bookshop.domain.BookMaster;
 import io.pivotal.bookshop.domain.Customer;
+import io.pivotal.bookshop.keys.OrderKey;
 
 public class GemFireClientCacheHelper {
-	
+
 	/**
-	 * Initializes the ClientCache. It also indirectly initializes the BookMaster and Customer regions by calling the associated
-	 * static methods.
+	 * Initializes the ClientCache. It also indirectly initializes the
+	 * BookMaster and Customer regions by calling the associated static methods.
 	 * 
 	 * @return
 	 */
+	// TODO-03: Modify the client cache creation code to also enable subscriptions
 	public static ClientCache create() {
 		ClientCache cache = new ClientCacheFactory()
-          .set("name", "Client App")
-          .addPoolLocator("localhost",10334)
-          .create();
-		
+				.set("name", "Client App")
+				.addPoolLocator("localhost", 10334)
+				.create();
+
 		createBookMasterClientRegion(cache);
 		createCustomerClientRegion(cache);
+		createBookOrderClientRegion(cache);
 		return cache;
 	}
-	
+
 	/**
-	 * Initializes the BookMaster region to act as a proxy to the server region by the same name AND caches a copy locally. 
-	 * Once created, it can later be fetched using the getRegion() method call on the ClientCache.
+	 * Initializes the BookMaster region to act as a caching proxy to the server
+	 * region by the same name AND caches a copy locally. Once created, it can
+	 * later be fetched using the getRegion() method call on the ClientCache.
 	 * 
 	 * @param cache
 	 */
 	private static void createBookMasterClientRegion(ClientCache cache) {
-		ClientRegionFactory<Integer, BookMaster> booksRegionFactory = cache.createClientRegionFactory(ClientRegionShortcut.CACHING_PROXY);
+		ClientRegionFactory<Integer, BookMaster> booksRegionFactory = cache
+				.createClientRegionFactory(ClientRegionShortcut.CACHING_PROXY);
 		booksRegionFactory.create("BookMaster");
 
 	}
-	
+
 	/**
-	 * Initializes the Customer region to act as a proxy to the server region by the same name AND caches a copy locally. 
-	 * Once created, it can later be fetched using the getRegion() method call on the ClientCache.
+	 * Initializes the Customer region to act as a caching proxy to the server
+	 * region by the same name AND caches a copy locally. Once created, it can
+	 * later be fetched using the getRegion() method call on the ClientCache.
 	 * 
 	 * @param cache
 	 */
 	public static void createCustomerClientRegion(ClientCache cache) {
-		ClientRegionFactory<Integer, Customer> customerRegionFactory = cache.createClientRegionFactory(ClientRegionShortcut.CACHING_PROXY);
+		ClientRegionFactory<Integer, Customer> customerRegionFactory = cache
+				.createClientRegionFactory(ClientRegionShortcut.CACHING_PROXY);
 		customerRegionFactory.create("Customer");
-		
+
 	}
+
+	/**
+	 * Initializes the BookOrder region to act as a proxy to the server region
+	 * by the same name. Once created, it can later be
+	 * fetched using the getRegion() method call on the ClientCache.
+	 * 
+	 * @param cache
+	 */
+	public static void createBookOrderClientRegion(ClientCache cache) {
+		ClientRegionFactory<OrderKey, Customer> customerRegionFactory = cache
+				.createClientRegionFactory(ClientRegionShortcut.PROXY);
+		customerRegionFactory.create("BookOrder");
+
+	}
+
+
 
 }
