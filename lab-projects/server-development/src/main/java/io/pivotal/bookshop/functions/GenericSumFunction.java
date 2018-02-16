96fb02f07e85492cd778d91273b1cb1405443290
Mark Secrist <msecrist@pivotal.io>
2018-02-16 08:57:46 -0700

    Revert "Re-commit of 12-Server-Functions-solution files"
    This reverts commit 393fdbec970c0a047d7b224c554aa985673fc134.

--- lab-projects/server-development/src/main/java/io/pivotal/bookshop/functions/GenericSumFunction.java
+++ lab-projects/server-development/src/main/java/io/pivotal/bookshop/functions/GenericSumFunction.java
@@ -10,49 +10,27 @@ import org.apache.geode.cache.execute.RegionFunctionContext;
 import org.apache.geode.cache.partition.PartitionRegionHelper;
 import org.apache.geode.pdx.PdxInstance;
 
-//TODO-01: Make this class implement the GemFire Function interface
-public class GenericSumFunction implements Function {
+// TODO-01: Make this class implement the GemFire Function interface
+public class GenericSumFunction  {
 
-	@Override
-	public void execute(FunctionContext context) {
 		// TODO-02: Ensure the FunctionContext is a RegionFunctionContext
-		if (context instanceof RegionFunctionContext) {
-			RegionFunctionContext rfc = (RegionFunctionContext) context;
 			// TODO-03: Get the argument from the FunctionContext representing the field to perform sum on
-			String fieldString = (String) rfc.getArguments();
-			// TODO-04: Use the PartitionRegionHelper to get all the local region data
-			Region<Object, PdxInstance> localRegion = PartitionRegionHelper.getLocalDataForContext(rfc);
-			BigDecimal summable = BigDecimal.ZERO;
-			// TODO-05: Iterate over the values in the local region data
-			for (PdxInstance item : localRegion.values()) {
-             // TODO-06: Get the requested field, assert it is a Numeric type, cast it and add it to
-             //          the summable variable defined above
-				Object field = item.getField(fieldString);
-				if ( field instanceof Float) {
-					summable = summable.add(BigDecimal.valueOf((Float) field));
-				} else {
-					System.out.println("Field : " + fieldString + " is NOT a Float. Value= " + field);
-				}
-			}
-			// TODO-07: Return the final sum
-			System.out.println("Returning: " + summable);
-			rfc.getResultSender().lastResult(summable);
-		} else {
-			throw new FunctionException("Function must be called as onRegion()");
-		}
 
-	}
+			// TODO-04: Use the PartitionRegionHelper to get all the local region data
+
+			// TODO-05: Iterate over the values in the local region data
+
+                // TODO-06: Get the requested field, assert it is a Numeric type, cast it and add it to
+                //          the summable variable defined above
+
+			// TODO-07: Return the final sum
+
+	
 
 	// TODO-08: Have this method return the name of the class as the simple name (i.e. just the class name)
-	@Override
-	public String getId() {
-		return getClass().getSimpleName();
-	}
 	
 	// TODO-09: Implement the method to force only primary buckets to be considered
-	@Override
-	public boolean optimizeForWrite() {
-		return true;
-	}
+	
+
 
 }
