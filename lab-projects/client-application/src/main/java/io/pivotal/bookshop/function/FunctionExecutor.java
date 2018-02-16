bf86be8ffe41b7335d2e0a2601f5e48d3bdc6afe
Mark Secrist <msecrist@pivotal.io>
2018-02-16 08:40:06 -0700

    Revert "Solution files for 13-Client-Functions-solution"
    This reverts commit d38b573efc2037904ca3b3aad56a0e2d3fcdad62.

--- lab-projects/client-application/src/main/java/io/pivotal/bookshop/function/FunctionExecutor.java
+++ lab-projects/client-application/src/main/java/io/pivotal/bookshop/function/FunctionExecutor.java
@@ -12,13 +12,12 @@ public class FunctionExecutor {
 	// TODO-05: Inspect the signature of this method
 	public static BigDecimal callSumFunction(Region region, String fieldName) {
 		// TODO-06: execute the function using the supplied 'fieldName' for the field and the supplied region to execute the function on
-		Execution execution = FunctionService.onRegion(region).setArguments(fieldName)
-				.withCollector(new SummingResultCollector());
 
-		ResultCollector rc = execution.execute("GenericSumFunction");
+
 
 		// TODO-07: Get result and assert that the two orders total $93.95
-		return (BigDecimal) rc.getResult();
+
+		return null;
 	
 	}
 }
